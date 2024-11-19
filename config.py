adca69288d010d81e8159707fedd03947bfa4d00
larinpaul <www.larinpavel@mail.ru>
2024-11-19 22:17:39 +0500

    Trying to connect with Migrate

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:postgres@localhost:5433/postgres'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:1234@localhost:5433/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # REPLACE 'user','password','localhost','dbname' wit your actual credentials
 
